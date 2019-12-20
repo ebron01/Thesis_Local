3001da2c8ee42622a20047ef456a80e66cc24be4
eboran <emreboran@hacettepe.edu.tr>
2019-12-20 15:31:07 +0300

    parser updated with order

--- parser-nltk/parsed_glove.py
+++ parser-nltk/parsed_glove.py
@@ -1,24 +1,24 @@
 import json
 import os
 import numpy as np
+import datetime
 import sys
 
+'''
+run with CLAS env
+'''
 try:
     import _pickle as pickle
 except:
+    # import pickle
+    # print ("Warning: You are running code with Python 2.xx.\n")
     print ("Error: Please run code with Python 3.xx.\n")
     sys.exit(1)
 
-import datetime
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-parsed_file = '/parsed_np_vp.json'
-option = 'pickle' # or 'numpy'
-start = datetime.datetime.now()
-
 def loadGloveModel(gloveFile):
     print("Loading Glove Model")
-    with open(gloveFile,'r+', encoding='utf-8') as f:
+    # with open(gloveFile,'r+') as f:
+    with open(gloveFile, 'r+', encoding='utf-8') as f:
         data = f.readlines()
     model = {}
     try:
@@ -29,13 +29,55 @@ def loadGloveModel(gloveFile):
             model[word] = embedding
     except Exception as e:
         print(e)
-    print("Done.",len(model)," words loaded!")
+    print("Done. " + str(len(model)) + " words loaded!\n")
     return model
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+parsed_file = '/sorted_parsed_np_vp.json'
+option = 'pickle' # or 'numpy'
+start = datetime.datetime.now()
 
 Model = loadGloveModel(dir_path + "/vectors.txt")
 
+# with open(dir_path + parsed_file, 'r+') as f:
 with open(dir_path + parsed_file, 'r', encoding='utf-8') as f:
     parsed_sentences_dict = json.load(f)
+print('Loaded parsed sentences of closest captions')
+
+#TODO:in progress:
+concurrence_d = {}
+for key in parsed_sentences_dict.keys():
+    np_list = []
+    vp_list = []
+    for k in parsed_sentences_dict[key].keys():
+        for NP in parsed_sentences_dict[key][k]['np']:
+            np_list.append(NP)
+        for VP in parsed_sentences_dict[key][k]['vp']:
+            vp_list.append(VP)
+    concurrence_d.update({key: {'np': np_list, 'vp': vp_list}})
+
+
+concurrence_count = {}
+for key in concurrence_d.keys():
+    concur_np = {}
+    concur_vp = {}
+    for _NP in concurrence_d[key]['np']:
+        count = 0
+        for _np in concurrence_d[key]['np']:
+            if str(_NP) == str(_np):
+                count += 1
+        concur_np.update({_NP: count})
+    for _VP in concurrence_d[key]['vp']:
+        count = 0
+        for _vp in concurrence_d[key]['vp']:
+            if str(_VP) == str(_vp):
+                count += 1
+        concur_vp.update({_VP: count})
+    concurrence_count.update({key: {'np': concur_np, 'vp': concur_vp}})
+
+with open('concurrence_np_vp.json', 'w') as f:
+    json.dump(concurrence_count, f)
 
 glove_dict = {}
 for key in parsed_sentences_dict.keys():
