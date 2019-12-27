67d176510fbf590756c2f3247f6dfdfd75a23bdd
eboran <emreboran@hacettepe.edu.tr>
2019-12-27 23:55:29 +0300

    plotter added.

--- adv-inf-master/plotter.py
+++ adv-inf-master/plotter.py
@@ -4,7 +4,7 @@ import ast
 import torch
 # sample_tensor = torch.rand(3)
 dir = '/Users/emre/Desktop/Dense_Visual/plots/'
-name = 'slurm-5079_out'
+name = 'slurm-5093_out'
 # num_epochs = int(name.split('_')[1].strip('epoch'))
 type = '.out'
 file = '/Users/emre/Desktop/'
@@ -13,13 +13,18 @@ with open(filename, 'r') as f:
     data = f.readlines()
 
 iter_line = []
+v_iter_line = []
 for line in data:
     if 'g_iter' in line:
         iter_line.append(line)
 
+    elif 'evaluating validation preformance... 11/' in line:
+        v_iter_line.append(line)
+
 epoch = []
 iter = []
 iter_loss = []
+epoch_loss = []
 c = 1
 i = 0
 for it in iter_line:
@@ -34,10 +39,35 @@ for it in iter_line:
     iter.append(g_iter)
     iter_loss.append(g_iter_loss)
 
+g_epoch_loss = []
+
+for j in range(len(epoch)):
+    start_index = epoch[j] * i
+    end_index = (epoch[j] + 1) * i
+    g_epoch_loss.append(sum(iter_loss[start_index:end_index]) / i)
+
+v_count = 0
+v_g_epoch_loss = []
+for it in v_iter_line:
+    print(v_count)
+    v_count += 1
+    v_iter_l = it.split(' ')
+    try:
+        v_g_epoch = float(ast.literal_eval(v_iter_l[4].strip('\n').strip('(').strip(')')))
+    except :
+        break
+    v_g_epoch_loss.append(v_g_epoch)
+
+
+
 for a in range(len(iter)):
     iter[a] = iter[a] / i
 
-plt.plot(iter, iter_loss, '-b', label='g_loss')
+x_for_v = range(len(v_g_epoch_loss))
+x_for_t = range(len(g_epoch_loss))
+# plt.plot(iter, iter_loss, '-b', label='g_loss')
+plt.plot(x_for_v, v_g_epoch_loss, '-r', label='v_g_loss')
+plt.plot(x_for_t, g_epoch_loss, '-b', label='v_g_loss')
 
 plt.xlabel(name)
 plt.legend(loc='upper left')
@@ -45,4 +75,5 @@ plt.title("losses")
 
 
 # plt.show() #to save the figure this line must be closed.
+name = 'closest10_encodesize10'
 plt.savefig(dir+name+''+'.png')
