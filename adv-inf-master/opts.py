1a6bc3fad90db897224f435366de468151da6ad8
eboran <emreboran@hacettepe.edu.tr>
2020-01-01 20:33:27 +0300

    eval_utils updated.

--- adv-inf-master/opts.py
+++ adv-inf-master/opts.py
@@ -173,7 +173,7 @@ def parse_opt():
                     help='If use box, do we normalize box feature')
 
     # Optimization: General
-    parser.add_argument('--g_pre_nepoch', type=int, default=1,
+    parser.add_argument('--g_pre_nepoch', type=int, default=15,
                     help='number of epochs to pre-train generator with cross entropy')
     parser.add_argument('--batch_size', type=int, default=16,
                     help='minibatch size')
@@ -226,7 +226,7 @@ def parse_opt():
                     help='How often do we want to print losses? (0 = disable)')
     parser.add_argument('--save_checkpoint_every', type=int, default=1,
                     help='how often to save a model checkpoint in iterations? the code already saves checkpoint every epoch (0 = dont save; 1 = every epoch')
-    parser.add_argument('--checkpoint_path', type=str, default='save_closest10_encodesize10_e1_lange1',
+    parser.add_argument('--checkpoint_path', type=str, default='save_e15_lange1_0101',
                     help='directory to store checkpointed models')
     parser.add_argument('--losses_log_every', type=int, default=25,
                     help='How often do we snapshot losses, for inclusion in the progress dump? (0 = disable)')
