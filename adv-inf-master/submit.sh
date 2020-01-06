ef7dabfd9830dd0e5f8f6b4552bf46ff1bda9f3a
ebron01 <emreboran@hacettepe.edu.tr>
2020-01-06 20:21:24 +0300

    submit changed.

--- adv-inf-master/submit.sh
+++ adv-inf-master/submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --workdir=/home/eboran/Thesis_Local/adv-inf-master/  		# working dir
-#SBATCH --gres=gpu:3  					# kac gpu reserve edilecek
+#SBATCH --gres=gpu:1  					# kac gpu reserve edilecek
 #SBATCH --output=/home/eboran/Thesis_Local/adv-inf-master/slurm-%j.out	# ciktilarin yazilacagi dosya
 #SBATCH --error=/home/eboran/Thesis_Local/adv-inf-master/slurm-%j.err	# hatalarin yazilacagi dosya
 #SBATCH --time=1-00:00:00				# isin max calisma zamani. han icin 1 gun, hanabi icin 2 gun.
@@ -13,4 +13,5 @@
 ####export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/samet/cuda-9.0/lib64
 #########################################################
 
-CUDA_LAUNCH_BLOCKING=1 python train.py --g_pre_nepoch 22 --g_start_from 'save_e15_lange1_b_32_0101' --g_start_epoch 'latest'
+#CUDA_VISIBLE_DEVICES="" python train.py --g_pre_nepoch 22 --g_start_from 'save_e15_lange1_b_32_0101' --g_start_epoch 'latest' --batch_size 16
+CUDA_LAUNCH_BLOCKING=1 python train.py --g_pre_nepoch 22 --g_start_from 'save_e15_lange1_b_32_0101' --g_start_epoch 'latest' --batch_size 16
