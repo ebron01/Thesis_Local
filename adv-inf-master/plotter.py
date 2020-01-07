1e9ec30b80c88b807cfd19baadfb648eaeaaad7e
eboran <emreboran@hacettepe.edu.tr>
2020-01-07 23:52:02 +0300

    plotter updated.

--- adv-inf-master/plotter.py
+++ adv-inf-master/plotter.py
@@ -4,7 +4,8 @@ import cPickle as pickle
 dir = '/Users/emre/Desktop/Advinf_Visual/'
 read_file = 'files/histories_lang1.pkl'
 save_file = 'plots/histories_lang1'
-plot_label = 'Closest captions: 10, Encode size for aux: 10 Batch size:32-16 '
+save_file_dis = 'plots/histories_lang1_dis'
+plot_label = 'Closest captions: 10, Encode size for aux: 10 Batch size:32-16'
 
 with open(dir + read_file, 'rb') as f:
     histories = pickle.load(f)
@@ -56,80 +57,80 @@ plt.title("Losses")
 plt.savefig(dir+save_file+''+'.png')
 
 
-d_epoch = []
-d_val_epoch = []
-for key in sorted(histories['d_loss_history'].keys()):
-    if histories['d_loss_history'][key]['d_epoch'] not in d_epoch:
-        d_epoch.append(histories['d_loss_history'][key]['d_epoch'])
-
-dis_v_loss = {}
-# dis_p_gen_accuracy = {}
-# dis_v_mm_accuracy = {}
-# dis_l_neg_accuracy = {}
-# dis_v_gen_accuracy = {}
-# dis_p_neg_accuracy = {}
-# dis_l_loss = {}
-# dis_l_gen_accuracy = {}
-# dis_p_loss = {}
-for e in d_epoch:
-    dis_v_loss_ = []
-    # dis_p_gen_accuracy_ = []
-    # dis_v_mm_accuracy_ = []
-    # dis_l_neg_accuracy_ = []
-    # dis_v_gen_accuracy_ = []
-    # dis_p_neg_accuracy_ = []
-    # dis_l_loss_ = []
-    # dis_l_gen_accuracy_ = []
-    # dis_p_loss_ = []
-    for key in sorted(histories['d_loss_history'].keys()):
-        if histories['d_loss_history'][key]['d_epoch'] == e:
-            dis_v_loss_.append(histories['d_loss_history'][key]['dis_v_loss'])
-            # dis_p_gen_accuracy_.append(histories['d_loss_history'][key]['dis_p_gen_accuracy'])
-            # dis_v_mm_accuracy_.append(histories['d_loss_history'][key]['dis_v_mm_accuracy'])
-            # dis_l_neg_accuracy_.append(histories['d_loss_history'][key]['dis_l_neg_accuracy'])
-            # dis_v_gen_accuracy_.append(histories['d_loss_history'][key]['dis_v_gen_accuracy'])
-            # dis_p_neg_accuracy_.append(histories['d_loss_history'][key]['dis_p_neg_accuracy'])
-            # dis_l_loss_.append(histories['d_loss_history'][key]['dis_l_loss'])
-            # dis_l_gen_accuracy_.append(histories['d_loss_history'][key]['dis_l_gen_accuracy'])
-            # dis_p_loss_.append(histories['d_loss_history'][key]['dis_p_loss_'])
-    dis_v_loss.update({e: dis_v_loss_})
-    # dis_p_gen_accuracy.update({e: dis_p_gen_accuracy_})
-    # dis_v_mm_accuracy.update({e: dis_v_mm_accuracy_})
-    # dis_l_neg_accuracy.update({e: dis_l_neg_accuracy_})
-    # dis_v_gen_accuracy.update({e: dis_v_gen_accuracy_})
-    # dis_p_neg_accuracy.update({e: dis_p_neg_accuracy_})
-    # dis_l_loss.update({e: dis_l_loss_})
-    # dis_l_gen_accuracy.update({e: dis_l_gen_accuracy_})
-    # dis_p_loss.update({e: dis_p_loss_})
-
-dis_v_loss_epoch = {}
-for key in dis_v_loss.keys():
-    summed = sum(dis_v_loss[key]) / len(dis_v_loss[key])
-    dis_v_loss_epoch.update({key: summed})
-
-d_loss_epoch_plot = []
-for key in dis_v_loss_epoch.keys():
-    d_loss_epoch_plot.append(dis_v_loss_epoch[key])
-
-d_val_epoch_loss = {}
-for e in histories['d_val_result_history'].keys():
-    d_val_epoch_loss.update({e: histories['d_val_result_history'][e]['d_loss']})
-
-d_val_epoch_loss_plot = []
-for key in d_val_epoch_loss.keys():
-    d_val_epoch_loss_plot.append(d_val_epoch_loss[key])
-
-d_for_v = range(len(d_val_epoch_loss_plot))
-d_for_t = range(len(d_loss_epoch_plot))
-# plt.plot(iter, iter_loss, '-b', label='g_loss')
-plt.plot(d_for_v, d_val_epoch_loss_plot, '-m', label='d_val_loss')
-plt.plot(d_for_t, d_loss_epoch_plot, '-c', label='d_train_loss')
-
+# d_epoch = []
+# d_val_epoch = []
+# for key in sorted(histories['d_loss_history'].keys()):
+#     if histories['d_loss_history'][key]['d_epoch'] not in d_epoch:
+#         d_epoch.append(histories['d_loss_history'][key]['d_epoch'])
 #
+# dis_v_loss = {}
+# # dis_p_gen_accuracy = {}
+# # dis_v_mm_accuracy = {}
+# # dis_l_neg_accuracy = {}
+# # dis_v_gen_accuracy = {}
+# # dis_p_neg_accuracy = {}
+# # dis_l_loss = {}
+# # dis_l_gen_accuracy = {}
+# # dis_p_loss = {}
+# for e in d_epoch:
+#     dis_v_loss_ = []
+#     # dis_p_gen_accuracy_ = []
+#     # dis_v_mm_accuracy_ = []
+#     # dis_l_neg_accuracy_ = []
+#     # dis_v_gen_accuracy_ = []
+#     # dis_p_neg_accuracy_ = []
+#     # dis_l_loss_ = []
+#     # dis_l_gen_accuracy_ = []
+#     # dis_p_loss_ = []
+#     for key in sorted(histories['d_loss_history'].keys()):
+#         if histories['d_loss_history'][key]['d_epoch'] == e:
+#             dis_v_loss_.append(histories['d_loss_history'][key]['dis_v_loss'])
+#             # dis_p_gen_accuracy_.append(histories['d_loss_history'][key]['dis_p_gen_accuracy'])
+#             # dis_v_mm_accuracy_.append(histories['d_loss_history'][key]['dis_v_mm_accuracy'])
+#             # dis_l_neg_accuracy_.append(histories['d_loss_history'][key]['dis_l_neg_accuracy'])
+#             # dis_v_gen_accuracy_.append(histories['d_loss_history'][key]['dis_v_gen_accuracy'])
+#             # dis_p_neg_accuracy_.append(histories['d_loss_history'][key]['dis_p_neg_accuracy'])
+#             # dis_l_loss_.append(histories['d_loss_history'][key]['dis_l_loss'])
+#             # dis_l_gen_accuracy_.append(histories['d_loss_history'][key]['dis_l_gen_accuracy'])
+#             # dis_p_loss_.append(histories['d_loss_history'][key]['dis_p_loss_'])
+#     dis_v_loss.update({e: dis_v_loss_})
+#     # dis_p_gen_accuracy.update({e: dis_p_gen_accuracy_})
+#     # dis_v_mm_accuracy.update({e: dis_v_mm_accuracy_})
+#     # dis_l_neg_accuracy.update({e: dis_l_neg_accuracy_})
+#     # dis_v_gen_accuracy.update({e: dis_v_gen_accuracy_})
+#     # dis_p_neg_accuracy.update({e: dis_p_neg_accuracy_})
+#     # dis_l_loss.update({e: dis_l_loss_})
+#     # dis_l_gen_accuracy.update({e: dis_l_gen_accuracy_})
+#     # dis_p_loss.update({e: dis_p_loss_})
+#
+# dis_v_loss_epoch = {}
+# for key in dis_v_loss.keys():
+#     summed = sum(dis_v_loss[key]) / len(dis_v_loss[key])
+#     dis_v_loss_epoch.update({key: summed})
+#
+# d_loss_epoch_plot = []
+# for key in dis_v_loss_epoch.keys():
+#     d_loss_epoch_plot.append(dis_v_loss_epoch[key])
+#
+# d_v_gen_acc_epoch = {}
+# for e in histories['d_val_result_history'].keys():
+#     d_v_gen_acc_epoch.update({e: histories['d_val_result_history'][e]['v_gen_accuracy']})
+#
+# d_val_epoch_loss_plot = []
+# for key in d_v_gen_acc_epoch.keys():
+#     d_val_epoch_loss_plot.append(d_v_gen_acc_epoch[key])
+#
+# d_for_v = range(len(d_val_epoch_loss_plot))
+# d_for_t = range(len(d_loss_epoch_plot))
+# # plt.plot(iter, iter_loss, '-b', label='g_loss')
+# plt.plot(d_for_v, d_val_epoch_loss_plot, '-m', label='d_v_gen_acc')
+# plt.plot(d_for_t, d_loss_epoch_plot, '-c', label='dis_v_loss')
+#
+# #
 # plt.xlabel(plot_label)
 # plt.legend(loc='upper left')
 # plt.title("Losses")
 #
 # # plt.show() #to save the figure this line must be closed.
-# plt.savefig(dir+save_file+''+'.png')
+# plt.savefig(dir+save_file_dis+''+'.png')
 print('Done')
