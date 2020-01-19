bbe41ef0cf05dfcc8049c30f69c4075c9fe1d8d7
eboran <emreboran@hacettepe.edu.tr>
2020-01-19 13:01:34 +0300

    bert created.

--- bert/bert.py
+++ bert/bert.py
@@ -1,14 +1,34 @@
 import json
-#from bert_serving.client import BertClient
-
-filename = '../parser-nltk/sorted_5closest_updated_query_mid.json'
-
-with open(filename, 'rb') as f:
+from bert_serving.client import BertClient
+import h5py
+import numpy as np
+filename = '../parser-nltk/sorted_10closest_updated_query_mid.json'
+filename_bert = '../parser-nltk/sorted_10closest_bert.pkl'
+with open(filename, 'r') as f:
     data = json.load(f)
 
+bc = BertClient()
+count = 0
+for key in data.keys():
+    if 'concap' not in key:
+        continue
+    count += 1
+    print(count)
+    encode_list = []
+    for k in data[key].keys():
+        encode_list.append(data[key][k]['caption'])
+    encoded = bc.encode(encode_list)
+    for k,v in enumerate(data[key].keys()):
+        data[key][v]['bert'] = encoded[k]
 
-print ('Done')
-
-#bc = BertClient()
-#bc.encode(['First do it', 'then do it right', 'then do it better'])
+with h5py.File('sample.h5', 'w') as f:
+    for key in data.keys():
+        if 'concap' not in key:
+            continue
+        grp = f.create_group(key)
+        for k in data[key].keys():
+            grp.create_dataset(name=k, shape=data[key][k]['bert'].shape,
+                       dtype=np.float32,
+                       data=data[key][k]['bert'])
+print('Done')
 
