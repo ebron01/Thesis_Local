86623593d2d822c48d0c8b9ca4a0a0ed2fcdf793
eboran <emreboran@hacettepe.edu.tr>
2020-01-27 21:34:28 +0300

    aux labels changed to torch tensor

--- adv_inf_master_v2/train_utils.py
+++ adv_inf_master_v2/train_utils.py
@@ -53,7 +53,7 @@ def train_discriminator(dis_model, gen_model, dis_optimizer, gan_crit, loader,
 
     with torch.no_grad():
 
-        aux_labels = np.zeros((loader.batch_size, loader.max_sent_num, loader.seq_length), dtype='int')
+        aux_labels = np.zeros((loader.batch_size, loader.max_sent_num, loader.seq_length), dtype='int64')
         count = 0
         for i in range(len(sent_num)):
             for j in range(sent_num[i]):
