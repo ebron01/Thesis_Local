b4fdc0738c676736a1fd1b2d9f5f3cf760b562d5
eboran <emreboran@hacettepe.edu.tr>
2020-01-27 20:45:44 +0300

    aux_ix added to discriminator 10 epochs cudas added.

--- adv_inf_master_v2/models/HybridDiscriminator.py
+++ adv_inf_master_v2/models/HybridDiscriminator.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
+import pdb
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -229,6 +229,7 @@ class MultiModalAttEarlyFusion(nn.Module):
             return moe_weights
 
     def forward(self, fc_feats, img_feats, box_feats, activity_labels, seq):
+        pdb.set_trace()
         batch_size = seq.size(0)
         sent_size = seq.size(1)
         scores = []
