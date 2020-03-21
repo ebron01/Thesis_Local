842beaca3d0ad5527cfe619107f0b7671f2a14ad
ebron01 <emreboran@hacettepe.edu.tr>
2020-03-21 14:31:32 +0300

    USE THIS: this takes codes to result_base_missing_wo_gt_full_sent. it uses  cc_np_vp_full_sent.npy.
    Signed-off-by: ebron01 <emreboran@hacettepe.edu.tr>

--- adv_inf_master_v2_concat/dataloader.py
+++ adv_inf_master_v2_concat/dataloader.py
@@ -339,6 +339,7 @@ class DataLoader(data.Dataset):
             # this part loads one np/vp word from cc dataset. It checks if there is a zero vector(created for np/vp longer than one word) for np/vp takes next vp/gt from cc caption.
             aux_label_batch[i, :, 1: self.seq_length + 1 ] = self.aux_np_vp_cc[ix]
 
+
             v_ix = self.video_id[ix]
 
             # get visually mismatched (mm) captions and features as inputs to generator and visual discriminator
