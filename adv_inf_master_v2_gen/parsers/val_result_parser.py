4b2b42f1412c001c9c35a2880c2e336016111f80
ebron01 <emreboran@hacettepe.edu.tr>
2020-04-01 23:24:08 +0300

    Updated val_result_parser.py
    Signed-off-by: ebron01 <emreboran@hacettepe.edu.tr>

--- adv_inf_master_v2_gen/parsers/val_result_parser.py
+++ adv_inf_master_v2_gen/parsers/val_result_parser.py
@@ -1,4 +1,6 @@
 import json
+import time
+
 # file_path = '/home/luchy/PycharmProjects/Thesis_Local/adv_inf_master_v2_gen/vis/vis_val_result_gen_embed_1_cc_npvp_vectors.json'
 val_gen_json = '/home/luchy/Desktop/results/result_base/vis/vis_val_base_k100.json'
 with open(val_gen_json, 'r') as f:
@@ -20,7 +22,7 @@ cc_parsed_closest1_json = 'parser_cc.json'
 with open(cc_parsed_closest1_json, 'r') as f:
     cc_parsed_closest1 = json.load(f)
 
-#this part creates paragraphs for each video from conceptual captions dataset for training ids of activitynet dataset
+# this part creates paragraphs for each video from conceptual captions dataset for training ids of activitynet dataset
 # cc_not_parsed_paragraph = {}
 # for train_key in train_gt_data.keys():
 #     cc_not_parsed = {}
@@ -42,7 +44,53 @@ with open(cc_parsed_closest1_json, 'r') as f:
 cc_not_parsed_paragraph_json = 'cc_not_parsed_paragraph_closest1.json'
 with open(cc_not_parsed_paragraph_json, 'r') as f:
     cc_not_parsed_paragraph = json.load(f)
+val_gen_concat_mmu_d19 = '/home/luchy/PycharmProjects/Thesis_Local/adv_inf_master_v2_concat/densevid_eval/caption_result_concat_mmu_19Mart_d19.json'
+with open(val_gen_concat_mmu_d19, 'r') as f:
+    val_gen_concat_mmu = json.load(f)
 
+val_gen_json_missing = '/home/luchy/PycharmProjects/Thesis_Local/adv_inf_master_v2_concat/densevid_eval/caption_result_base_missing_21Mart.json'
+with open(val_gen_json_missing, 'r') as f:
+    val_gen_data_missing = json.load(f)
 
+val_sent = ''
+val_sent_missing = ''
+val_gen_concat_mmu_sent = ''
+val_id = 'v_5SNtTQZnN4g'
+
+print(
+    '----------------------------------------------------------------------------------------------------------------------')
+print(val_id)
+print(
+    '----------------------------------------------------------------------------------------------------------------------')
+print('ground truth:')
+print(train_gt_data[val_id])
+print('\n')
+
+for i in range(len(val_gen_concat_mmu['results'][val_id])):
+    val_gen_concat_mmu_sent += val_gen_concat_mmu['results'][val_id][i]['sentence'] + '.'
+print('CC Concat MMU prediction:')
+print (val_gen_concat_mmu_sent)
+print('\n')
+
+for i in range(len(val_gen_data['results'][val_id])):
+    val_sent += val_gen_data['results'][val_id][i]['sentence'] + '.'
+print('CC Concat prediction:')
+print(val_sent)
+print('\n')
+
+for i in range(len(val_gen_data_missing['results'][val_id])):
+    val_sent_missing += val_gen_data_missing['results'][val_id][i]['sentence'] + '.'
+print('Base Model (Missing) Prediction:')
+print (val_sent_missing)
+print('\n')
+
+print('not parsed')
+print (cc_not_parsed_paragraph[val_id])
+print('\n')
+
+print('parsed')
+print (cc_parsed_closest1[val_id])
+print('\n')
+time.sleep(3)
 
 print('Done')
